5772f2ee2a58148e72f95ed16026ffc96bafc9f4
windcsy <wind.choi@wind-choi>
2021-03-18 16:19:04 +0900

    times

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,31 +3,31 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) {
-		
-		return MAX_DAYS[month-1];
+
+		return MAX_DAYS[month - 1];
 	}
 
-	public static void main(String[] args) {		
-		int a;
+	public static void main(String[] args) {
+		int a, times;
 		Scanner scanner = new Scanner(System.in);
 		String s1;
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요");
-		//s1 = scanner.next();
 
-		//a = Integer.parseInt(s1);
-		
-		a = scanner.nextInt();
-		System.out.printf("%d 월은 %d 일 입니다",a,cal.getMaxDaysOfMonth(a));
-		//if(a == 1 || a == 3 || a== 5 || a== 7 || a==8 || a==10 ||a == 12)
-	//		System.out.printf("%d 월은 31일 입니다", a);
-		//else if(a == 2)
-		//	System.out.printf("%d 월은 28일 입니다", a);
-		//else
-		//	System.out.printf("%d 월은 30일 입니다", a);
+		System.out.println("횟수를 입력하세요");
+		times = scanner.nextInt();
+
+		for (int i = 0; i < times; i++) {
+			System.out.println("달을 입력하세요");
+			a = scanner.nextInt();
+
+			System.out.printf("%d 월은 %d 일 입니다\n", a, cal.getMaxDaysOfMonth(a));
+		}
+
+		System.out.println("bye");
 		scanner.close();
 	}
 }
